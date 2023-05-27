0250714954c185c05806a76a6535919e035554d4
Michael B. Espeña <michael.espena.public@gmail.com>
2023-05-27 13:56:54 +0800

    lesson 10

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,14 +1,35 @@
 <script>
   let showModal = true;
+  let isPromo = true;
 </script>
 
 {#if showModal}
-  <div class="backdrop">
+  <div class="backdrop" class:promo={isPromo}>
     <div class="modal">
       <p>Sign up for offers!</p>
     </div>
   </div>
-
-  <style>
-  </style>
 {/if}
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+  }
+
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: whitesmoke;
+  }
+
+  .promo .modal {
+    background: crimson;
+    color: whitesmoke;
+  }
+</style>
